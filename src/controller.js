f8458d933d4b4428929db058c7e60c93002f6281
cahindo <erikfaderik@gmail.com>
2022-05-09 13:59:54 +0700

    feat: init chatbox UI

--- src/controller.js
+++ src/controller.js
@@ -1,32 +1,79 @@
-import jquery from "jquery";
+import $ from "jquery";
 import Konva from "konva";
 
-// first we need to create a stage
+/**
+ * Global Variables
+ */
+let players = [];
+const charSize = 80;
+
+/**
+ * Stage : Ukuran canvas dan propertinya
+ */
 var stage = new Konva.Stage({
-  container: 'canvaslayout',   // id of container <div>
-  width: 500,
-  height: 500
+  container: 'canvascontainer',
+  width: 1100,
+  height: 700
 });
 
-// then create layer
-var layer = new Konva.Layer();
+/**
+ * Base Layer : Tembok
+ */
+var baseLayer = new Konva.Layer();
 
-// create our shape
-var circle = new Konva.Circle({
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  radius: 70,
-  fill: 'red',
-  stroke: 'black',
-  strokeWidth: 4
+Konva.Image.fromURL('/assets/tembok.png', function (tembok) {
+  tembok.setAttrs({
+    x: 0,
+    y: 0,
+    scaleX: 1,
+    scaleY: 1,
+  });
+  baseLayer.add(tembok);
 });
 
-// add the shape to the layer
-layer.add(circle);
 
-// add the layer to the stage
-stage.add(layer);
+/**
+ * Game Layer : Player, Enemy, Bullet
+ */
+var gameLayer = new Konva.Layer();
 
-// draw the image
-layer.draw();
-layer.draw();
+function initPlayer(name, num) {
+  var charObj = new Image();
+  charObj.src = '/assets/char'+num+'.png';
+  charObj.onload = function () {
+    var char = new Konva.Image({
+      x: randomX(),
+      y: randomY(),
+      image: charObj,
+      width: charSize,
+      height: charSize,
+    });
+
+    baseLayer.add(char);
+  };
+}
+
+initPlayer('Player 1', 1);
+initPlayer('Player 2', 2);
+initPlayer('Player 3', 3);
+initPlayer('Player 4', 4);
+
+/**
+ * Stage Adder : Tambahkan layer ke stage
+ */
+
+stage.add(baseLayer);
+// stage.add(gameLayer)
+
+
+/**
+ * Helper : Anything
+ */
+
+function randomY() {
+  return Math.random() * (stage.height()-charSize*2) + charSize;
+}
+
+function randomX() {
+  return Math.random() * (stage.width()-charSize*2) + charSize;
+}
