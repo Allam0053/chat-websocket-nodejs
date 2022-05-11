38aa3b64d43031a9fcb37159e3c4640e07083162
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2022-05-11 15:26:19 +0700

    fix:no joining twice

--- server/server.js
+++ server/server.js
@@ -1,47 +1,51 @@
 const app = require("express")();
 
-const PORT_CLIENT = 8080
-const PORT_SERVER = 8081
+const PORT_CLIENT = 8080;
+const PORT_SERVER = 8081;
 
 // app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"));
-app.listen(PORT_CLIENT, () => console.log("Client listening on port " + PORT_CLIENT));
+app.listen(PORT_CLIENT, () =>
+    console.log("Client listening on port " + PORT_CLIENT)
+);
 
 const express = require("express");
 const http = require("http");
 const websocketServer = require("websocket").server;
 const httpServer = http.createServer();
-httpServer.listen(PORT_SERVER, () => console.log("Server listening on port " + PORT_SERVER));
+httpServer.listen(PORT_SERVER, () =>
+    console.log("Server listening on port " + PORT_SERVER)
+);
 
 const clients = {};
 const rooms = {};
 
 const wsServer = new websocketServer({
-    "httpServer": httpServer
-})
+    httpServer: httpServer,
+});
 
-wsServer.on("request", request => {
+wsServer.on("request", (request) => {
     // connect
     const connection = request.accept(null, request.origin);
-    connection.on("open", () => console.log("opened!"))
-    connection.on("close", () => console.log("closed!"))
-    connection.on("message", message => {
-        const result = JSON.parse(message.utf8Data)
+    connection.on("open", () => console.log("opened!"));
+    connection.on("close", () => console.log("closed!"));
+    connection.on("message", (message) => {
+        const result = JSON.parse(message.utf8Data);
         // a client want to create a new room
         if (result.method === "create") {
             const clientId = result.clientId;
             const roomId = guidRoom();
             rooms[roomId] = {
-                "id": roomId,
-                "clients": [{"clientId":clientId}],
-                "messages": []
-            }
+                id: roomId,
+                clients: [{ clientId: clientId }],
+                messages: [],
+            };
 
             const payLoad = {
-                "method": "create",
-                "room" : rooms[roomId]
-            }
+                method: "create",
+                room: rooms[roomId],
+            };
 
-            console.log(clientId + ' has created #' + roomId);
+            console.log(clientId + " has created #" + roomId);
 
             const con = clients[clientId].connection;
             con.send(JSON.stringify(payLoad));
@@ -49,25 +53,26 @@ wsServer.on("request", request => {
 
         // a client want to join
         if (result.method === "join") {
-
             const clientId = result.clientId;
             const roomId = result.roomId;
             const room = rooms[roomId];
-            
-            room.clients.push({
-                "clientId": clientId,
-            })
+
+            if (!isPlayerJoined(roomId, clientId)) {
+                room.clients.push({
+                    clientId: clientId,
+                });
+            }
 
             const payLoad = {
-                "method": "join",
-                "room": room
-            }
-            console.log(clientId + ' has joined #' + roomId);
+                method: "join",
+                room: room,
+            };
+            console.log(clientId + " has joined #" + roomId);
 
             // loop through all clients and tell them that people has joined
-            room.clients.forEach(c => {
-                clients[c.clientId].connection.send(JSON.stringify(payLoad))
-            })
+            room.clients.forEach((c) => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad));
+            });
         }
 
         // chat
@@ -76,7 +81,7 @@ wsServer.on("request", request => {
             const clientId = result.clientId;
             const roomId = result.roomId;
             const room = rooms[roomId];
-            
+
             // rooms[roomId].messages.push(
             //     {
             //         "sender": clientId,
@@ -86,59 +91,71 @@ wsServer.on("request", request => {
 
             // Sent back the messages to all client in the same room
             const payLoad = {
-                "method": "message",
-                "messages": {
-                    "sender": clientId,
-                    "message": result.message
-                }
-            }
+                method: "message",
+                messages: {
+                    sender: clientId,
+                    message: result.message,
+                },
+            };
 
-            room.clients.forEach(c => {
-                clients[c.clientId].connection.send(JSON.stringify(payLoad))
-            })
-
+            room.clients.forEach((c) => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad));
+            });
         }
-    })
+    });
 
     // generate a new clientId
     const clientId = guidUser();
     clients[clientId] = {
-        "connection":  connection
-    }
+        connection: connection,
+    };
 
     const payLoad = {
-        "method": "connect",
-        "clientId": clientId
-    }
+        method: "connect",
+        clientId: clientId,
+    };
 
     //send back the client connect
-    connection.send(JSON.stringify(payLoad))
-})
+    connection.send(JSON.stringify(payLoad));
+});
 
+/**
+ *
+ * @param {*} roomId | number
+ * @param {*} clientId | number
+ * @returns true if player exists in the room | boolean
+ */
+function isPlayerJoined(roomId, clientId) {
+    const room = rooms[roomId];
+    if (
+        typeof room.clients.find((o) => o.clientId === clientId) === "undefined"
+    ) {
+        return false;
+    }
+    return true;
+}
 
-function updateRoomState(){
+function updateRoomState() {
     for (const g of Object.keys(rooms)) {
-        const room = rooms[g]
+        const room = rooms[g];
         const payLoad = {
-            "method": "update",
-            "room": room
-        }
+            method: "update",
+            room: room,
+        };
 
-        room.clients.forEach(c=> {
-            clients[c.clientId].connection.send(JSON.stringify(payLoad))
-        })
+        room.clients.forEach((c) => {
+            clients[c.clientId].connection.send(JSON.stringify(payLoad));
+        });
     }
 
     setTimeout(updateGameState, 500);
 }
 
-
-
 function S4() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 }
- 
+
 // then to call it, plus stitch in '4' in the third group
-const guidRoom = () => ('R' + S4()).toUpperCase();
-const guidUser = () => 'User' +( S4() ).toLowerCase();
+const guidRoom = () => ("R" + S4()).toUpperCase();
+const guidUser = () => "User" + S4().toLowerCase();
 // const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
